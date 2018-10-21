7699eb632a6ee4c3f0384e9afd5f459bdec40eed
Shane <carrollshaned@gmail.com>
2018-10-20 22:52:07 -0400

    more work on react

--- src/js/containers/RPECalculator.tsx
+++ src/js/containers/RPECalculator.tsx
@@ -21,73 +21,42 @@ export interface RPECalculatorProps {
 
 type Validator<T> = (value: T) => string | null;
 
+interface RPEInputProps<T> {
+    id: string;
+    label: string;
+    value: T;
+    validator: Validator<T>;
+    onChange: (id: string, value: T) => void;
+
+    inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+}
+
+function RPEInput<T extends string | number | string[]>(props: RPEInputProps<T>) {
+
+    const validationError = props.validator(props.value);
+
+    const handleChange = (ev: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+
+        const input: HTMLInputElement | HTMLSelectElement = ev.target;
+
+        props.onChange(props.id, input.value as T);
+    };
+
+    return <div className={`input-row given-weight ${validationError ? "error" : ""}`}>
+        <div className="error">{validationError}</div>
+        <div className="input-container">
+            <label htmlFor={props.id}> {props.label} </label>
+            <input id={props.id}{...props.inputProps} value={props.value} onChange={handleChange}/>
+        </div>
+    </div>
+}
+
 export class RPECalculator extends React.Component<RPECalculatorProps, RPECalculatorState> {
 
-    static validators: {[id: string]: Validator<any> } = {
-        "givenWeight": (value: number) => {
-            if (value !== null && value !== undefined && value <= 0) {
-                return "Weight must be above 0";
-            }
-            return null;
-        }
-    };
-
-    validateWeight = (inputRow, errors: { [id: string]: string }) => {
-
-        const input = inputRow.querySelector("input");
-        const inputValue = input.value;
-
-        if (inputValue.length > 0 && inputValue <= 0) {
-            errors[input.getAttribute("id")] = "Weight must be above 0";
-        }
-    };
-
-    validateRPE = (inputRow, errors: { [id: string]: string }) => {
-
-        const input = inputRow.querySelector("input");
-        const inputValue = input.value;
-
-        const id = input.getAttribute("id");
-
-        if (inputValue.length > 0 && inputValue < 6) {
-            errors[id] = "RPE must be 6 or above";
-        } else if (inputValue.length > 0 && inputValue > 10) {
-            errors[id] = "RPE must be 10 or below";
-        }
-
-    };
-
-    validateReps = (inputRow, errors: { [id: string]: string }) => {
-        const input = inputRow.querySelector("input");
-        const inputValue = input.value;
-
-        const id = input.getAttribute("id");
-
-        if (inputValue.length > 0 && inputValue <= 0) {
-            errors[id] = "Reps must be above 0";
-        } else if (inputValue.length > 0 && inputValue > 12) {
-            errors[id] = "Sorry, this calculator only goes up to 12 reps";
-        }
-    };
-
-/*    sanitizeAndGetInputErrors = () => {
-        const errors: { [id: string]: string } = {};
-
-        const inputRowsArr = [].slice.call(document.querySelectorAll(".input-row"));
-        inputRowsArr.forEach(function (inputRow) {
-            if (inputRow.className.indexOf("weight") > 0) {
-                this.validateWeight(inputRow, errors);
-            } else if (inputRow.className.indexOf("rpe") > 0) {
-                this.validateRPE(inputRow, errors);
-            } else if (inputRow.className.indexOf("reps") > 0) {
-                this.validateReps(inputRow, errors);
-            }
-        });
-
-        this.setState({errors});
-
-        return errors;
-    };*/
+    constructor(props) {
+        super(props);
+        this.state = {errors: {}, roundingValue: 5.0};
+    }
 
     roundToFloat = (value: number, round: number): string => {
 
@@ -105,26 +74,27 @@ export class RPECalculator extends React.Component<RPECalculatorProps, RPECalcul
         }
     };
 
-    constructor(props) {
-        super(props);
-        this.state = { errors: {}, roundingValue: 5.0 };
-    }
-
-    hasError(id: string): boolean {
-        return (id in this.state.errors) && !!this.state.errors[id];
-    }
-
-    handleInputChange = (ev: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-
-        const input: HTMLInputElement | HTMLSelectElement = ev.target;
+    handleInputChange = (id: string, value: any) => {
 
         let stateChanges = {
-            [input.id]: input.value as any,
+            [id]: value,
         } as RPECalculatorState;
 
         this.setState(stateChanges);
     };
 
+    repValidator = (value: string) => {
+        if (value !== null && value !== undefined) {
+            const reps = parseInt(value);
+            if(reps <= 0){
+                return "Reps must be greater than 0";
+            }
+            if(reps > 12){
+                return "Sorry, this calculator only goes up to 12 reps";
+            }
+        }
+        return null;
+    };
 
     render() {
 
@@ -138,9 +108,6 @@ export class RPECalculator extends React.Component<RPECalculatorProps, RPECalcul
         const desiredRPEDecimal: number = (this.state.desiredRPE && this.state.desiredReps) ?
             RPE.getFactor(this.state.desiredRPE, this.state.desiredReps) :
             Number.NaN;
-
-        const givenWeightError = RPECalculator.validators.givenWeight(this.state.givenWeight);
-
         return <div id="content">
             <div className="header">
                 <h1> RPE Calculator </h1>
@@ -150,39 +117,46 @@ export class RPECalculator extends React.Component<RPECalculatorProps, RPECalcul
                 <h3> Basis Numbers </h3>
             </div>
 
-            <div className={`input-row given-weight ${givenWeightError ? "error" : ""}`}>
-                <div className="error">{givenWeightError}</div>
-                <div className="input-container">
-                    <label htmlFor="given-weight"> Weight </label>
-                    <input id="givenWeight" type="number" step="0.01" className="weight" value={this.state.givenWeight} onChange={this.handleInputChange}/>
-                </div>
-            </div>
+            <RPEInput id={"givenWeight"} label={"Weight"} value={this.state.givenWeight} onChange={this.handleInputChange} inputProps={{type: "number", step: "0.01", className: "weight"}}
+                      validator={(value: number) => {
+                          if (value !== null && value !== undefined && value <= 0) {
+                              return "Weight must be above 0";
+                          }
+                          return null;
+                      }}
+            />
 
-            <div className={`input-row given-reps ${this.hasError("givenReps")}`}>
-                <div className="error"></div>
-                <div className="input-container">
-                    <label htmlFor="given-reps"> Reps </label>
-                    <input id="givenReps" type="number" className="reps" value={this.state.givenReps} onChange={this.handleInputChange}/>
-                </div>
-            </div>
+            <RPEInput id={"givenReps"} label="Reps" value={this.state.givenReps} onChange={this.handleInputChange} inputProps={{type: "number", className: "reps"}}
+                      validator={this.repValidator}
+            />
 
-            <div className="input-row given-rpe bottom-border">
-                <div className="error"></div>
-                <div className="input-container">
-                    <label htmlFor="given-rpe"> RPE </label>
-                    <input id="givenRPE" type="number" className="rpe" value={this.state.givenRPE} onChange={this.handleInputChange}/>
-                </div>
-            </div>
+            <RPEInput id={"givenRPE"} label="RPE" value={this.state.givenRPE} onChange={this.handleInputChange} inputProps={{type: "number", className: "rpe"}}
+                      validator={(value: string) => {
+                          if (value !== null && value !== undefined) {
+                              const rpe = parseInt(value);
+                              if(rpe < 6 || rpe > 10){
+                                  return "RPE must be at least 6 and no more than 10";
+                              }
+                          }
+                          return null;
+                      }}
+            />
 
             <div className="subheader">
                 <h3> Target Numbers </h3>
             </div>
 
+            <RPEInput id={"desiredReps"} label="Reps" value={this.state.desiredReps} onChange={this.handleInputChange}
+                      inputProps={{type: "number", className: "reps", disabled: !allGiven}}
+                      validator={this.repValidator}
+            />
+
             <div className="input-row desired-reps">
                 <div className="error"></div>
                 <div className="input-container">
                     <label htmlFor="desired-reps"> Reps </label>
-                    <input id="desiredReps" type="number" className="reps" disabled={!allGiven} value={this.state.desiredReps} onChange={this.handleInputChange}/>
+                    <input id="desiredReps" type="number" className="reps" disabled={!allGiven}
+                           value={this.state.desiredReps} onChange={this.handleInputChange}/>
                 </div>
             </div>
 
@@ -190,23 +164,33 @@ export class RPECalculator extends React.Component<RPECalculatorProps, RPECalcul
                 <div className="error"></div>
                 <div className="input-container">
                     <label htmlFor="desired-rpe"> RPE </label>
-                    <input id="desiredRPE" type="number" className="rpe" disabled={!allGiven} value={this.state.desiredRPE} onChange={this.handleInputChange}/>
+                    <input id="desiredRPE" type="number" className="rpe" disabled={!allGiven}
+                           value={this.state.desiredRPE} onChange={this.handleInputChange}/>
                 </div>
             </div>
 
             <div className="results">
-                <h3> Target weight: <span id="solved-weight"> {this.roundToFloat(estimated1RM * desiredRPEDecimal, this.state.roundingValue)} </span> </h3>
-                <h3> 100% of e1RM: <span id="e1RM"> {this.roundToFloat(estimated1RM, this.state.roundingValue)} </span> </h3>
-                <h3> 95% of e1RM: <span id="ninetyFiveP"> {this.roundToFloat(estimated1RM * 0.95, this.state.roundingValue)} </span></h3>
-                <h3> 85% of e1RM: <span id="eightyFiveP"> {this.roundToFloat(estimated1RM * 0.85, this.state.roundingValue)} </span></h3>
-                <h3> 80% of e1RM: <span id="eightyP"> {this.roundToFloat(estimated1RM * 0.80, this.state.roundingValue)} </span></h3>
-                <h3> 75% of e1RM: <span id="seventyFiveP"> {this.roundToFloat(estimated1RM * 0.75, this.state.roundingValue)} </span></h3>
-                <h3> 65% of e1RM: <span id="sixtyFiveP"> {this.roundToFloat(estimated1RM * 0.65, this.state.roundingValue)} </span></h3>
+                <h3> Target weight: <span
+                    id="solved-weight"> {this.roundToFloat(estimated1RM * desiredRPEDecimal, this.state.roundingValue)} </span>
+                </h3>
+                <h3> 100% of e1RM: <span id="e1RM"> {this.roundToFloat(estimated1RM, this.state.roundingValue)} </span>
+                </h3>
+                <h3> 95% of e1RM: <span
+                    id="ninetyFiveP"> {this.roundToFloat(estimated1RM * 0.95, this.state.roundingValue)} </span></h3>
+                <h3> 85% of e1RM: <span
+                    id="eightyFiveP"> {this.roundToFloat(estimated1RM * 0.85, this.state.roundingValue)} </span></h3>
+                <h3> 80% of e1RM: <span
+                    id="eightyP"> {this.roundToFloat(estimated1RM * 0.80, this.state.roundingValue)} </span></h3>
+                <h3> 75% of e1RM: <span
+                    id="seventyFiveP"> {this.roundToFloat(estimated1RM * 0.75, this.state.roundingValue)} </span></h3>
+                <h3> 65% of e1RM: <span
+                    id="sixtyFiveP"> {this.roundToFloat(estimated1RM * 0.65, this.state.roundingValue)} </span></h3>
             </div>
 
             <div className="options">
                 <label htmlFor="rounding" className="rounding"> Rounding: </label>
-                <select id="roundingValue" className="rounding" name="rounding" value={this.state.roundingValue} onChange={this.handleInputChange}>
+                <select id="roundingValue" className="rounding" name="rounding" value={this.state.roundingValue}
+                        onChange={this.handleInputChange}>
                     <option value="5">5.0</option>
                     <option value="2.5">2.5</option>
                     <option value="1">1.0</option>
