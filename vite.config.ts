bd854574cbdce3161fea381851e86e6bd74f6911
Zack Youngren <github.com@youngren.io>
2026-01-01 09:33:54 -0500

    pwa (#19)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VitePWA({ registerType: 'autoUpdate' }),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
